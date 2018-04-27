7f46eef4f235a2ffe3c76616c7367bfbf01aae33
andy.patterson <andy.patterson@genesys.com>
2018-04-27 11:41:51 -0400

    feat: comply with fantasy-land specs
    By complying with fantasy-land specifications we can maximize the
    reusability of this lib across other libraries. The spec itself is
    reasonably straight-forward, we just need to expose a few methods
    with an alias. Because fantasy-land doesn't expose types, it isn't
    currently possible to import their definitions for use.
    
    closes: #5

---
+++ src/fantasy-land.ts
@@ -0,0 +1,6 @@
+// TODO: remove this if fantasy-land exports own types
+
+export const map = 'fantasy-land/map';
+export const ap = 'fantasy-land/ap';
+export const of = 'fantasy-land/of';
+export const chain = 'fantasy-land/chain';
